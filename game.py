05ea89b6025d82d4b6756fa0511dadd492aa3dff
Student <user@chromebox-001.(none)>
2014-02-18 12:32:01 -0800

    Completed steps 1 to 4

--- game.py
+++ game.py
@@ -10,9 +10,9 @@ DEBUG = False
 KEYBOARD = None
 PLAYER = None
 ######################
-
-GAME_WIDTH = 3
-GAME_HEIGHT = 3
+#increase the GRID size - step3, step4
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
 
 #### Put class definitions here ####
 class Rock(GameElement):
@@ -22,25 +22,31 @@ class Rock(GameElement):
 
 def initialize():
     #"""Put game initialization code here"""
-   
-   #increase the GRID size - step3, step4
-    GAME_WIDTH = 4
-    GAME_HEIGHT = 4
+ 
+    rock_positions = [(2,1),(1,2),(3,2),(2,3)]
+    rocks = []
 
-   #Intialise and register Rock1 - step2 
-    rock1 = Rock()
-    GAME_BOARD.register(rock1) #step1
-    GAME_BOARD.set_el(1,1,rock1) #step1
+    for pos in rock_positions:
+        rock = Rock()
+        GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(pos[0],pos[1],rock)
+        rocks.append(rock)
 
-    #Intialise and register Rock2 - step2
-    rock2 = Rock()
-    GAME_BOARD.register(rock2)
-    GAME_BOARD.set_el(2,2,rock2)
 
-    print "The first rock is at", (rock1.x, rock1.y)
-    print "The second rock is at", (rock2.x, rock2.y)
-    print "Rock 1 image", rock1.IMAGE
-    print "Rock 2 image", rock2.IMAGE
+   # #Intialise and register Rock1 - step2 
+   #  rock1 = Rock()
+   #  GAME_BOARD.register(rock1) #step1
+   #  GAME_BOARD.set_el(1,1,rock1) #step1
+
+   #  #Intialise and register Rock2 - step2
+   #  rock2 = Rock()
+   #  GAME_BOARD.register(rock2)
+   #  GAME_BOARD.set_el(2,2,rock2)
+
+   #  print "The first rock is at", (rock1.x, rock1.y)
+   #  print "The second rock is at", (rock2.x, rock2.y)
+   #  print "Rock 1 image", rock1.IMAGE
+   #  print "Rock 2 image", rock2.IMAGE
 
 
 
